15782a3e5a9ae4b83810ab8984c3cc1dd4f4b88b
Alireza Same <alirezasame@gmail.com>
2025-03-18 10:50:35 +0330

    trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-'use server';
-
-
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/theme-toggle";
 
 export default async function Home() {
-
   return (
     <main className="min-h-screen p-8">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold mb-4">
           Welcome to My Next.js Application
         </h1>
-        
+        <div>
+          <Button>Click me</Button>
+          <ModeToggle />
+        </div>
         <div className="bg-white p-6 rounded-lg shadow-md">
-          <p className="mb-4">
-            This is a sample Next.js page with a counter.
-          </p>
+          <p className="mb-4">This is a sample Next.js page with a counter.</p>
         </div>
       </div>
     </main>
